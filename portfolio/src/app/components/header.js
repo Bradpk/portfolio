31304e5919db8ea38e02f32f065359ae76e2e03f
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-01-27 21:11:48 -0500

    g

---
+++ portfolio/src/app/components/header.js
@@ -0,0 +1,22 @@
+import styles from "./header.module.css";
+
+function Header() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.content}>
+                    <h1>BRADLEY KERR</h1>
+                    <p>FULL STACK SOFTWARE DEVELOPER</p>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#intro">Projects</a></li>
+                    <li><a href="#work">Blog</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
