89ef33b89ce99b7a2fe744f0b488cc9d3c617848
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-01-31 22:27:04 -0500

    Making home page responsive

--- portfolio/src/app/components/navbar.js
+++ portfolio/src/app/components/navbar.js
@@ -3,22 +3,22 @@ import styles from './navbar.module.css';
 
 function Navbar() {
     return (
-        <nav className={`navbar navbar-expand-md nav-dark ${styles.navbar}`}>
+        <nav className={`navbar navbar-expand-sm ${styles.navbar}`}>
             <div className='container-fluid'>
-                <button className='navbar-toggler' data-toggle='collapse' data-target='#main-nav'><span className='navbar-toggler-icon'></span></button>
-                <div className='collapse navbar-collapse justify-content-center align-center' id='main-nav'>
-                    <ul className='navbar-nav'>
-                        <li className='nav-item'><Link className='nav-link' href='/'>HOME</Link></li>
-                        <li className='nav-item'><Link className='nav-link' href='/projects'>PROJECTS</Link></li>
-                        <li className='nav-item'><Link className='nav-link' href='/about'>BLOG</Link></li>
-                        <li className='nav-item'><Link className='nav-link' href='/about'>ABOUT</Link></li>
-                        <li className='nav-item'><Link className='nav-link' href='/contact'>CONTACT</Link></li>
+                <button type='button' className='navbar-toggler' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle Navigation'>
+                <span className={`navbar-toggler-icon ${styles.togglerIcon}`}></span>
+                </button>
+                <div className='collapse navbar-collapse justify-content-center align-center' id='navbarNav'>
+                    <ul className={`navbar-nav ${styles.navoptions}`}>
+                        <li className='nav-item'><Link className='nav-link text-light' href='/'>HOME</Link></li>
+                        <li className='nav-item'><Link className='nav-link text-light' href='/projects'>PROJECTS</Link></li>
+                        <li className='nav-item'><Link className='nav-link text-light' href='/about'>BLOG</Link></li>
+                        <li className='nav-item'><Link className='nav-link text-light' href='/about'>ABOUT</Link></li>
+                        <li className='nav-item'><Link className='nav-link text-light' href='/contact'>CONTACT</Link></li>
                     </ul>
                 </div>
             </div>
         </nav>
-
-
     );
 }
 
