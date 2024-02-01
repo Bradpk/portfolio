62e0b9235a039eb7e6989189a03fa9f97f829947
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-01-31 19:07:04 -0500

    g

---
+++ portfolio/src/app/components/projects.js
@@ -0,0 +1,12 @@
+import styles from './projects.module.css';
+
+function Projects () {
+    return (
+        <div>
+            <h1 className={styles.test}>Projects</h1>
+            <p>Here are some of my projects</p>
+        </div>
+    );
+}
+
+export default Projects;
--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/header";
+import Navbar from "./components/navbar";
+import Projects from "./components/projects";
 
 export default function Home() {
   return (
   <>
+  <Navbar />
   <Header />
   </>
   );
