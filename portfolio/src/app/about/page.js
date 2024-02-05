ba3c1c12586718b01140f9439a0e54f0c3c55860
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-05 13:35:33 -0500

    Adding needed information

--- portfolio/src/app/about/page.js
+++ portfolio/src/app/about/page.js
@@ -1,11 +1,22 @@
 import Navbar from "../components/navbar";
+import styles from './about.module.css';
 
 function About() {
   return (
     <div>
     <Navbar />
-      <h1>About</h1>
-      <p>These are my projects</p>
+    <div className={`container ${styles.aboutcontainer}`}>
+      <p>I was born and raised in Auckland, New Zealand. After graduating from high school, I attended
+					college to pursue a degree in audio production. Following my college graduation, I had a
+					desire to travel and spent time exploring Europe while working in the yachting
+					industry. In 2019, I relocated to the United States after obtaining permanent residency. Music
+					has played a significant role as both a hobby and a career. I've devoted much of my adult life
+					to
+					music production and music education. I also enjoy hiking, fishing, rock climbing and grabbing a
+					drink with my mates whenever I get the chance.<br></br><br></br>While my passion for music remains, I've
+					discovered a new interest in programming that has led me to pursue a fresh career path in the tech
+					industry.</p>
+    </div>
     </div>
   );
 }
--- portfolio/src/app/projects/page.js
+++ portfolio/src/app/projects/page.js
@@ -1,11 +1,91 @@
 import Navbar from "../components/navbar";
+import Image from "next/image";
+import styles from './projects.module.css';
 
 function Projects() {
   return (
     <div>
-    <Navbar />
-      <h1>Projects</h1>
-      <p>These are my projects</p>
+      <Navbar />
+      <div className={`container ${styles.projectitems}`}>
+        <div className='row justify-content-center text-center'>
+          <div className='col-4'>
+          <h1>EAR ELEVATION</h1>
+					<p class="description">A game designed for musicians to improve their auditory recognition</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
+						<Image class="project" src='/ear-elevation.png' width={240} height={160} alt="img"></Image></a>
+					<a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+          </div>
+
+          <div className='col-4'>
+          <h1>VIRTUAL SYNTH</h1>
+					<p class="description">A virtual musical synthesizer with controls for oscillator selection and
+						envelope settings</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
+						<Image class="project" src='/ear-elevation.png' width={240} height={160} alt="img"></Image></a>
+					<a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+          </div>
+
+          <div className='col-4'>
+          <h1>TIK-TAC-TOE</h1>
+					<p class="description">A rendition of the classic game Tic-Tac-Toe<br></br>Give it
+						a go!</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
+						<Image class="project" src='/ear-elevation.png' width={240} height={160} alt="img"></Image></a>
+					<a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+          </div>
+
+        </div>
+        <div className='row justify-content-center text-center'>
+
+          <div className='col-4'>
+          <h1>TO DO LIST</h1>
+					<p class="description">A rendition of the classic game Tic-Tac-Toe / Noughts and Crosses <br></br>Give it
+						a go!</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
+						<Image class="project" src='/ear-elevation.png' width={240} height={160} alt="img"></Image></a>
+					<a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+          </div>
+
+          <div className='col-4'>
+          <h1>WEATHER APP</h1>
+					<p class="description">A rendition of the classic game Tic-Tac-Toe / Noughts and Crosses <br></br>Give it
+						a go!</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
+						<Image class="project" src='/ear-elevation.png' width={240} height={160} alt="img"></Image></a>
+					<a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+          </div>
+
+          <div className='col-4'>
+          <h1>REACT RESTURANT</h1>
+					<p class="description">A rendition of the classic game Tic-Tac-Toe / Noughts and Crosses <br></br>Give it
+						a go!</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
+						<Image class="project" src='/ear-elevation.png' width={240} height={160} alt="img"></Image></a>
+					<a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+          </div>
+
+        </div>
+        <div className='row justify-content-center text-center'>
+
+          <div className='col-4'>
+          <h1>ALARM CLOCK</h1>
+					<p class="description">A rendition of the classic game Tic-Tac-Toe / Noughts and Crosses <br></br>Give it
+						a go!</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
+						<Image class="project" src='/ear-elevation.png' width={240} height={160} alt="img"></Image></a>
+					<a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+          </div>
+
+          <div className='col-4'>
+          <h1>STORY GENERATOR</h1>
+					<p class="description">A rendition of the classic game Tic-Tac-Toe / Noughts and Crosses <br></br>Give it
+						a go!</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
+						<Image class="project" src='/ear-elevation.png' width={240} height={160} alt="img"></Image></a>
+					<a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+          </div>
+
+          <div className='col-4'>
+          <h1>MIND READER</h1>
+					<p class="description">A rendition of the classic game Tic-Tac-Toe / Noughts and Crosses <br></br>Give it
+						a go!</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
+						<Image class="project" src='/ear-elevation.png' width={240} height={160} alt="img"></Image></a>
+					<a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
