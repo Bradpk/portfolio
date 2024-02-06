ca4894c694a5412c5799fe206e7919be50a446e9
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-06 13:41:40 -0500

    Updating bio

--- portfolio/src/app/about/page.js
+++ portfolio/src/app/about/page.js
@@ -12,10 +12,10 @@ function About() {
           college to pursue a degree in audio production. Following my college graduation, I had a
           desire to travel and spent time exploring Europe while working in the yachting
           industry. In 2019, I relocated to the United States after obtaining permanent residency. Music
-          has played a significant role as both a hobby and a career. I've devoted much of my adult life
+          has played a significant role in my life as both a hobby and a career. I've devoted much of my adult life
           to
-          music production and music education. I also enjoy hiking, fishing, rock climbing and grabbing a
-          drink with friends whenever I get the chance.<br></br><br></br>While my passion for music remains, I've
+          music production and music education. When I'm not doing something music related or coding I also enjoy hiking, fishing, rock climbing and grabbing a
+          drink with friends whenever I get the chance.<br></br><br></br>Over the past eighteen months or so I've
           discovered a new interest in programming that has led me to pursue a fresh career path in the tech
           industry.</p>
 
