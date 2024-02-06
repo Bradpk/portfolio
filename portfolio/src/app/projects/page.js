351415e6718eeeccc7d979c316b82a65d81ec032
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-06 11:21:24 -0500

    Adding in relavant links and images for projects page

--- portfolio/src/app/projects/page.js
+++ portfolio/src/app/projects/page.js
@@ -25,73 +25,73 @@ function Projects() {
               <div className='justify-content-center text-center'>
                 <h1>EAR ELEVATION</h1>
                 <p class="description">A game designed for musicians to improve their auditory recognition</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
-                  <Image src='/ear-elevation.png' width={450} height={300} alt="img"></Image></a>
-                <a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+                  <Image src='/ear-elevation.png' width={450} height={300} alt="project screenshot"></Image></a>
+                <a href="https://github.com/Bradpk/Ear_Elevation_Front_Two" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
             <div class="carousel-item">
               <div className='justify-content-center text-center'>
                 <h1>VIRTUAL SYNTHESIZER</h1>
-                <p class="description">A virtual musical synthesizer with controls for oscillator selection and envelope settings</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
-                  <Image src='/ear-elevation.png' width={450} height={300} alt="img"></Image></a>
-                <a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+                <p class="description">A virtual musical synthesizer with controls for oscillator selection and envelope settings</p><a href="https://synth-pi.vercel.app/piano/" target='_blank'>
+                  <Image src='/synth.png' width={450} height={300} alt="project screenshot"></Image></a>
+                <a href="https://github.com/Bradpk/Synth" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
             <div class="carousel-item">
               <div className='justify-content-center text-center'>
                 <h1>TIK-TAC-TOE</h1>
                 <p class="description">A rendition of the classic game Tic-Tac-Toe
-                  Give it a go!</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
-                  <Image src='/ear-elevation.png' width={450} height={300} alt="img"></Image></a>
-                <a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+                  Give it a go!</p><a href="https://bradpk.github.io/tic-tac-toe//" target='_blank'>
+                  <Image src='/tictactoe.png' width={450} height={300} alt="project screenshot"></Image></a>
+                <a href="https://github.com/Bradpk/tic-tac-toe" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
             <div class="carousel-item">
               <div className='justify-content-center text-center'>
                 <h1>TO DO LIST</h1>
-                <p class="description">A to-do list, because you keep forgetting that you need onions from Kroger </p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
-                  <Image src='/ear-elevation.png' width={450} height={300} alt="img"></Image></a>
-                <a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+                <p class="description">A to-do list, because you keep forgetting that you need onions from Kroger </p><a href="https://bradpk.github.io/to-do-list//" target='_blank'>
+                  <Image src='/todolist.png' width={450} height={300} alt="project screenshot"></Image></a>
+                <a href="https://github.com/Bradpk/to-do-list" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
             <div class="carousel-item">
               <div className='justify-content-center text-center'>
                 <h1>WEATHER APP</h1>
-                <p class="description">A weather app that allows you to fetch weather information from a given zip code</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
-                  <Image src='/ear-elevation.png' width={450} height={300} alt="img"></Image></a>
-                <a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+                <p class="description">A weather app that allows you to fetch weather information from a given zip code</p><a href="https://bradpk.github.io/weather-app//" target='_blank'>
+                  <Image src='/weatherapp.png' width={450} height={300} alt="project screenshot"></Image></a>
+                <a href="https://github.com/Bradpk/weather-app" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
             <div class="carousel-item">
               <div className='justify-content-center text-center'>
                 <h1>REACT RESTAURANT</h1>
-                <p class="description">A mock restaurant website I created to practice making HTTP requests</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
-                  <Image src='/ear-elevation.png' width={450} height={300} alt="img"></Image></a>
-                <a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+                <p class="description">A mock restaurant website I created to practice making HTTP requests</p><a href="https://bradpk.github.io/React-Restaurant-/#/" target='_blank'>
+                  <Image src='/bistro.png' width={450} height={300} alt="project screenshot"></Image></a>
+                <a href="https://github.com/Bradpk/React-Restaurant-" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
             <div class="carousel-item">
               <div className='justify-content-center text-center'>
                 <h1>ALARM CLOCK</h1>
-                <p class="description">An alarm clock designed to get you up and at 'em. But whats the magic button do?</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
-                  <Image src='/ear-elevation.png' width={450} height={300} alt="img"></Image></a>
-                <a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+                <p class="description">An alarm clock designed to get you up and at 'em. But whats the magic button do?</p><a href="https://bradpk.github.io/alarm-clock/" target='_blank'>
+                  <Image src='/alarmclock.png' width={450} height={300} alt="project screenshot"></Image></a>
+                <a href="https://github.com/Bradpk/alarm-clock" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
             <div class="carousel-item">
               <div className='justify-content-center text-center'>
                 <h1>STORY GENERATOR</h1>
-                <p class="description">A random story generator with options for names and measurements</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
-                  <Image src='/ear-elevation.png' width={450} height={300} alt="img"></Image></a>
-                <a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+                <p class="description">A random story generator with options for names and measurements</p><a href="https://bradpk.github.io/silly-story-generator/" target='_blank'>
+                  <Image src='/sillystory.png' width={450} height={300} alt="project screenshot"></Image></a>
+                <a href="https://github.com/Bradpk/silly-story-generator" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
             <div class="carousel-item">
               <div className='justify-content-center text-center'>
                 <h1>MIND READER</h1>
-                <p class="description">A project that guesses the number you picked in your head, whoa..</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
-                  <Image src='/ear-elevation.png' width={450} height={300} alt="img"></Image></a>
-                <a href="https://github.com/Bradpk/Ear_Elevation_Front_Two"><br></br><br></br>Github Repo</a>
+                <p class="description">A project that guesses the number you picked in your head, whoa..</p><a href="https://bradpk.github.io/mind-reader/" target='_blank'>
+                  <Image src='/mindreader.png' width={450} height={300} alt="project screenshot"></Image></a>
+                <a href="https://github.com/Bradpk/mind-reader" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
           </div>
