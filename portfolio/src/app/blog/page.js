8d7611507427f996374cfacea05670f84bc28f9e
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-06 11:50:34 -0500

    Working on centering the carousel text

--- portfolio/src/app/blog/page.js
+++ portfolio/src/app/blog/page.js
@@ -6,8 +6,8 @@ function Blog() {
     <div>
     <Navbar />
     <div className={`container ${styles.blogcontainer}`}>
-    <div id="carouselExampleCaptions" class="carousel slide">
-          <div class="carousel-indicators">
+    <div id="carouselExampleCaptions" className={`carousel slide ${styles.carousel}`}>
+          <div className={`carousel-indicators ${styles.indicators}`}>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
@@ -26,8 +26,40 @@ function Blog() {
           <div class="carousel-inner">
             <div class="carousel-item active">
               <div className='justify-content-center text-center'>
-                <h4>WEEK ONE</h4>
-                <p>A game designed for musicians to improve their auditory recognition</p>
+              <h1>(WEEK ONE)</h1>
+
+        <h1>Was your first week what you expected? Why? Why not?</h1>
+
+        <p> Going into this, I didn't have any preconceptions about what the bootcamp would be like or how it would be
+            structured, so I can't say either way that it was or wasn't what I expected. I think the boot camp strikes a
+            good balance between self problem solving which prepares us of what an actual job environment would be like,
+            yet
+            having the tutors there to help us out if we hit a blocker. What struck me out of the gate, is how quickly I
+            am
+            going to have to adapt to being in a learning environment again. The last time I was in school was about 8
+            years
+            ago so it's been a fair minute. It's a little intimidating but I am up for a challenge and I'm curious to
+            see
+            how well I will be able to retain all this new information. Big Ups to the tutors and the rest of the class,
+            everybody has been very supportive and helpful. </p>
+
+        <h1>What are you excited or eager to learn more about?</h1>
+
+        <p>The thing I am looking forward to the most is getting to the point where I have a big picture understanding
+            of
+            the entire process. Right now it feels like I am doing a jigsaw puzzle, not yet aware of the picture that
+            will
+            be created. I want to be able to create an app or interactive website from go to whoa! If I had to be more
+            specific though, I'm looking forward to both <strong>Javascript</strong> and <strong>Python</strong> as they
+            seem to be the nuts and bolts of coding. </p>
+
+        <h1>What is something about you that can be learned by reading this blog?</h1>
+
+        <p> I am originally from New Zealand and moved here impulsively in 2019 after winning a green card in a lottery.
+            My
+            primary hobbies are playing guitar, rock climbing and just recently coding. One thing people may not know
+            about
+            me is that I use to work as a deckhand in the superyacht industry in Europe. </p>
               </div>
             </div>
             <div class="carousel-item">
