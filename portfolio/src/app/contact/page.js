6f9cb95ce91479738b517e4ac585ee1de49f575c
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-06 13:25:06 -0500

    Adding icon links

--- portfolio/src/app/contact/page.js
+++ portfolio/src/app/contact/page.js
@@ -10,21 +10,21 @@ function Contact() {
         <div className={`row ${styles.row}`}>
           <div className='col-6'>
             <p>Github</p>
-            <Image src='/github.png' width={100} height={98} alt="github logo" className={styles.image}></Image>
+            <a target="_blank" href="https://github.com/Bradpk"><Image src='/github.png' width={100} height={98} alt="github logo" className={styles.image}></Image></a>
           </div>
           <div className='col-6'>
             <p>Linkedin</p>
-            <Image src='/linkedin.png' width={100} height={85} alt="github logo" className={styles.image}></Image>
+            <a target="_blank" href="https://www.linkedin.com/in/brad-kerr/"><Image src='/linkedin.png' width={100} height={85} alt="github logo" className={styles.image}></Image></a>
           </div>
         </div>
         <div className={`row ${styles.row}`}>
           <div className='col-6'>
             <p>Resume</p>
-            <Image src='/resume.png' width={100} height={100} alt="github logo" className={styles.image}></Image>
+            <a target="_blank" href="https://docs.google.com/document/d/1fgNX_K_nG4fsWiiYutB4Qmi2pEdzLylWTs-th9_6S_s/edit"><Image src='/resume.png' width={100} height={100} alt="github logo" className={styles.image}></Image></a>
           </div>
           <div className='col-6'>
             <p>Email</p>
-            <Image src='/email.png' width={100} height={100} alt="github logo" className={styles.image}></Image>
+            <a target="_blank" href="mailto:Brad.pk@live.com"><Image src='/email.png' width={100} height={100} alt="github logo" className={styles.image}></Image></a>
           </div>
         </div>
       </div>
