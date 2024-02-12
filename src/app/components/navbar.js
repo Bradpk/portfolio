021cb56126a9c0cc7559652f81991afe5df29bed
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-12 13:38:36 -0500

    Chaning navbar icon color to white

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
                 <button type='button' className='navbar-toggler' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle Navigation'>
                     <span className={`navbar-toggler-icon ${styles.togglerIcon}`}></span>
                 </button>
-                <div className='collapse navbar-collapse justify-content-center align-center' id='navbarNav'>
+                <div className='collapse navbar-collapse justify-content-center' id='navbarNav'>
                     <ul className={`navbar-nav ${styles.navoptions}`}>
                         <li className='nav-item'><Link className='nav-link text-light' href='/'>HOME</Link></li>
                         <li className='nav-item'><Link className='nav-link text-light' href='/projects'>PROJECTS</Link></li>
