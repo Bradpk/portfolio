e55fe8f9b39af64c4ac5b7e8032bdea4ac4f5ca7
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-13 18:29:31 -0500

    fixing little spelling mistake

--- src/app/components/template.js
+++ src/app/components/template.js
@@ -5,8 +5,8 @@ import { motion } from 'framer-motion'
 export default function Template({ children }) {
     return (
         <motion.div
-            initial={{ x: 0, opacity: 0 }}
-            animate={{ x: 0, opacity: 1, transition: { duration: 1 } }}
+            initial={{ x: 0, opacity: 0}}
+            animate={{ x: 0, opacity: 1, transition: { duration: 1} }}
         >
             {children}
         </motion.div>
