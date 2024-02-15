c900090d9045ffa53bbfed0b6d14e81fc1095456
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-15 15:12:28 -0500

    Adding tab title

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,18 @@ import styles from "./page.module.css";
 import Header from "./components/header";
 import Navbar from "./components/navbar";
 import Template from "./components/template";
+import Head from "next/head";
 
 export default function Home() {
   return (
-  <>
-  <Navbar />
-  <Template>
-  <Header />
-  </Template>
-  </>
+    <>
+      <Head>
+        <title>Bradley K</title>
+      </Head>
+      <Navbar />
+      <Template>
+        <Header />
+      </Template>
+    </>
   );
 }
