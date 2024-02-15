e13411c4467966e5600a7c1b9b4cb6ed525d2d97
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-15 16:38:47 -0500

    Undoing the before title and fixing it

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,10 @@ import styles from "./page.module.css";
 import Header from "./components/header";
 import Navbar from "./components/navbar";
 import Template from "./components/template";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Bradley K</title>
-      </Head>
       <Navbar />
       <Template>
         <Header />
