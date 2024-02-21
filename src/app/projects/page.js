cd6be9e115b79d4ca243503a4fa9c57c942bd874
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-21 16:26:22 -0500

    updating new project pics

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -26,7 +26,7 @@ function Projects() {
               <div className='text-center'>
                 <h1>EAR ELEVATION</h1>
                 <p>A game designed for musicians to improve their auditory recognition</p><a href="https://ear-elevation-ni65itt2f-bradpk.vercel.app/" target='_blank'>
-                  <Image className={styles.test} src='/ear-elevation.png' width={450} height={300} alt="project screenshot"></Image></a><br></br>
+                  <Image className={styles.test} src='/ear-elevation.png' width={460} height={300} alt="project screenshot"></Image></a><br></br>
                 <a className={styles.github} href="https://github.com/Bradpk/Ear_Elevation_Front_Two" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
@@ -34,7 +34,7 @@ function Projects() {
               <div className='text-center'>
                 <h1>VIRTUAL SYNTHESIZER</h1>
                 <p>A synthesizer with controls for oscillator selection and envelope settings</p><a href="https://synth-pi.vercel.app/piano/" target='_blank'>
-                  <Image src='/synth.png' width={450} height={300} alt="project screenshot"></Image></a><br></br>
+                  <Image src='/synth.png' width={460} height={300} alt="project screenshot"></Image></a><br></br>
                 <a className={styles.github} href="https://github.com/Bradpk/Synth" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
@@ -42,7 +42,7 @@ function Projects() {
               <div className='text-center'>
                 <h1>TIK-TAC-TOE</h1>
                 <p>A rendition of the classic game Tic-Tac-Toe or Noughts and Crosses</p><a href="https://bradpk.github.io/tic-tac-toe//" target='_blank'>
-                  <Image src='/tictactoe.png' width={450} height={300} alt="project screenshot"></Image></a><br></br>
+                  <Image src='/tictactoe.png' width={460} height={300} alt="project screenshot"></Image></a><br></br>
                 <a className={styles.github} href="https://github.com/Bradpk/tic-tac-toe" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
@@ -50,7 +50,7 @@ function Projects() {
               <div className='text-center'>
                 <h1>TO DO LIST</h1>
                 <p>A to-do list, because you keep forgetting that you need onions from Kroger </p><a href="https://bradpk.github.io/to-do-list//" target='_blank'>
-                  <Image src='/todolist.png' width={450} height={300} alt="project screenshot"></Image></a><br></br>
+                  <Image src='/todo.png' width={460} height={300} alt="project screenshot"></Image></a><br></br>
                 <a className={styles.github} href="https://github.com/Bradpk/to-do-list" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
@@ -58,7 +58,7 @@ function Projects() {
               <div className='text-center'>
                 <h1>WEATHER APP</h1>
                 <p>A weather app that allows you to fetch weather data from a given zip code</p><a href="https://bradpk.github.io/weather-app//" target='_blank'>
-                  <Image src='/weatherapp.png' width={450} height={300} alt="project screenshot"></Image></a><br></br>
+                  <Image src='/weatherapp.png' width={460} height={300} alt="project screenshot"></Image></a><br></br>
                 <a className={styles.github} href="https://github.com/Bradpk/weather-app" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
@@ -66,7 +66,7 @@ function Projects() {
               <div className='text-center'>
                 <h1>REACT RESTAURANT</h1>
                 <p>A mock restaurant website created to practice making HTTP requests</p><a href="https://bradpk.github.io/React-Restaurant-/#/" target='_blank'>
-                  <Image src='/bistro.png' width={450} height={300} alt="project screenshot"></Image></a><br></br>
+                  <Image src='/bistro.png' width={460} height={300} alt="project screenshot"></Image></a><br></br>
                 <a className={styles.github} href="https://github.com/Bradpk/React-Restaurant-" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
@@ -74,7 +74,7 @@ function Projects() {
               <div className='text-center'>
                 <h1>ALARM CLOCK</h1>
                 <p>An alarm clock to get you up and at 'em. Whats the magic button do?</p><a href="https://bradpk.github.io/alarm-clock/" target='_blank'>
-                  <Image src='/alarmclock.png' width={450} height={300} alt="project screenshot"></Image></a><br></br>
+                  <Image src='/alarmclock.png' width={460} height={300} alt="project screenshot"></Image></a><br></br>
                 <a className={styles.github} href="https://github.com/Bradpk/alarm-clock" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
@@ -82,7 +82,7 @@ function Projects() {
               <div className='text-center'>
                 <h1>STORY GENERATOR</h1>
                 <p>A random story generator with options for names and measurements</p><a href="https://bradpk.github.io/silly-story-generator/" target='_blank'>
-                  <Image src='/sillystory.png' width={450} height={300} alt="project screenshot"></Image></a><br></br>
+                  <Image src='/story.png' width={460} height={300} alt="project screenshot"></Image></a><br></br>
                 <a className={styles.github} href="https://github.com/Bradpk/silly-story-generator" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
@@ -90,7 +90,7 @@ function Projects() {
               <div className='text-center'>
                 <h1>MIND READER</h1>
                 <p>A project that guesses the number you picked in your head, whoa..</p><a href="https://bradpk.github.io/mind-reader/" target='_blank'>
-                  <Image src='/mindreader.png' width={450} height={300} alt="project screenshot"></Image></a><br></br>
+                  <Image src='/mindreader.png' width={460} height={300} alt="project screenshot"></Image></a><br></br>
                 <a className={styles.github} href="https://github.com/Bradpk/mind-reader" target='_blank'><br></br><br></br>Github Repo</a>
               </div>
             </div>
