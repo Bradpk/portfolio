10c92f07c5b479a43da15aec681e48d867f8e85a
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-21 17:14:35 -0500

    g

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -17,9 +17,10 @@ function About() {
           has played a significant role in my life as both a hobby and a career. I've devoted much of my adult life
           to
           music production and music education. When I'm not doing something music related or coding I also enjoy hiking, fishing, rock climbing and grabbing a
-          drink with friends whenever I get the chance.<br></br>Over the past eighteen months or so I've
+          drink with friends whenever I get the chance. Over the past eighteen months or so I've
           discovered a new interest in programming that has led me to pursue a fresh career path in the tech
           industry.</p>
+          <p>Feb 2024<br></br>Currently job searching and always happy to chat! Get in touch if you'd like to know more. </p>
       </div>
       </Template>
     </div>
