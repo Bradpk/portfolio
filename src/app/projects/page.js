9998a7f8b57be1c08f53401404d1f4eb9e8a8eb3
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-04-08 13:09:21 -0400

    g

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -50,7 +50,7 @@ function Projects() {
               <div class="carousel-item">
                 <div className='text-center'>
                   <h1>DOTNET CALCULATOR</h1>
-                  <p>A basic calculator created with asp.net core framework</p><a href="https://dotnet-calculator.azurewebsites.net/" target='_blank'>
+                  <p>A basic calculator created with the asp.net core framework</p><a href="https://dotnet-calculator.azurewebsites.net/" target='_blank'>
                     <Image className={styles.test} src='/calculator.png' width={460} height={300} alt="project screenshot"></Image></a><br></br>
                   <a className={styles.github} href="https://github.com/Bradpk/dotnet-calculator" target='_blank'><br></br><br></br>Github Repo</a>
                 </div>
